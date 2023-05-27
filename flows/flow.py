38714cf949d768a98b80f29be67f375e80a405b5
mbaddar <mbaddar2@gmail.com>
2023-05-28 00:57:01 +0200

    first yaml flow for qa over faq.txt file

--- flows/flow.py
+++ flows/flow.py
@@ -1,14 +1,35 @@
-# get a token: https://huggingface.co/docs/api-inference/quicktour#get-your-api-token
+"""
+build a flow for QA via LLM and RAG
+ref
+https://python.langchain.com/en/latest/modules/chains/index_examples/question_answering.html
+"""
 import argparse
 import logging
-from langchain.document_loaders import TextLoader
+from typing import List
 import yaml
-from tqdm import tqdm
 from langchain import HuggingFaceHub
 from langchain.llms import OpenAI
-from langchain.prompts.prompt import PromptTemplate
-from langchain.chains.llm import LLMChain
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Chroma
 from langchain.chains.question_answering import load_qa_chain
+from tqdm import tqdm
+
+
+def write_qa(header: str, out_file_path: str, questions: List[str], answers: List[str]) -> None:
+    out_file = open(out_file_path, "w")
+    out_file.write(f'#\n{header}\n#\n')
+    out_file.write("\n")
+    assert len(questions) == len(answers)
+    N = len(questions)
+    for i in range(N):
+        out_file.write(f"Q:{questions[i].strip()}")
+        out_file.write("\n")
+        out_file.write(f"A:{answers[i].strip()}")
+        out_file.write("\n")
+        out_file.write("---")
+        out_file.write("\n")
+    out_file.close()
 
 
 def get_hugging_face_llm_interface(config: dict, llm_name_id_map):
@@ -30,12 +51,6 @@ def get_openai_llm_interface(config: dict):
     return OpenAI(model_name=model_id)
 
 
-"""
-refs
-https://python.langchain.com/en/latest/modules/models/llms/integrations/huggingface_hub.html
-"""
-
-
 def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--q-file', type=str, required=True,
@@ -49,40 +64,54 @@ def get_parser():
     return parser
 
 
-def run_pipeline(config: dict, args):
-    llm = None
+def get_docs_retriever(config: dict, context_doc: str):
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_text(context_doc)
+
+    embeddings = OpenAIEmbeddings()  # fixme, decide it from embeddings
+    docs_retriever = Chroma.from_texts(texts, embeddings,
+                                       metadatas=[{"source": str(i)} for i in range(len(texts))]).as_retriever()
+    return docs_retriever
+
+
+def run_faq_pipeline(llm, docs_retriever, questions):
+    logger_ = logging.getLogger()
+    logger_.info('Starting faq pipeline')
+    answers = []
+    for question in tqdm(questions, desc='answering questions'):
+        docs = docs_retriever.get_relevant_documents(question)
+        chain = load_qa_chain(llm, chain_type="stuff")
+        answer = chain.run(input_documents=docs, question=question).strip()
+        answers.append(answer)
+    assert len(questions) == len(answers)
+    return answers
+
+    # write answers
+
+
+def run_master_pipeline(config: dict, context_doc: str, questions: List[str]) -> List[str]:
+    """
+
+    :param config:
+    :param questions:
+    :return:
+    """
+    logger.info(f'Getting LLM')
     if config['model']['provider'] == 'huggingface':
         llm = get_hugging_face_llm_interface(config=config, llm_name_id_map=config['model_map'])
     elif config['model']['provider'] == 'openai':
         llm = get_openai_llm_interface(config=config)
     else:
         raise ValueError("Invalid provider")
-    loader = TextLoader(args.docs, encoding='utf8')
-    documents = loader.load()
-    chain = load_qa_chain(llm, chain_type="stuff")
-    answers = []
-    in_file = open(args.q_file, "r")
-    questions = in_file.readlines()
-    in_file.close()
-    # answer questions
-    N = len(questions)
-    for question in tqdm(questions, desc='answering questions'):
-        question = question.strip()
-        if config['prediction'] == 'chain':
-            answer = chain.run(input_documents=documents, question=question).strip()
-        elif config['prediction'] == 'llm':
-            answer = llm.predict(question).strip()
-        else:
-            raise ValueError('Unknown prediction methods')
-        # answer = llm.predict(question).strip()
-        answers.append(answer)
-    assert len(questions) == len(answers)
-    # write answers
-    with open(args.qa_file, "w") as fo:
-        for i in range(N):
-            str_to_write = f"Q:{questions[i].strip()}\nA:{answers[i].strip()}\n---\n"
-            fo.write(str_to_write)
-        fo.close()
+    logger.info(f'Specifying task')
+    if config['task'] == 'faq':
+        docs_retriever = get_docs_retriever(config, context_doc)
+        answers = run_faq_pipeline(llm, docs_retriever, questions)
+        return answers
+
+
+    else:
+        raise ValueError(f"unsupported task {config['task']}")
 
 
 if __name__ == '__main__':
@@ -94,4 +123,12 @@ if __name__ == '__main__':
     #
     with open(args.flow) as stream:
         config = yaml.safe_load(stream)
-    run_pipeline(config, args)
+    # get questions
+    q_file = open(args.q_file, "r")
+    questions = q_file.readlines()
+    context_doc_file = open(args.docs, "r")
+    context_doc = context_doc_file.read()
+    logger.info(f'Staring master pipeline')
+    answers = run_master_pipeline(config=config, context_doc=context_doc, questions=questions)
+    write_qa(out_file_path=args.qa_file, header="", questions=questions, answers=answers)
+    logger.info(f'Finished writing resulted QA to {args.qa_file}')
