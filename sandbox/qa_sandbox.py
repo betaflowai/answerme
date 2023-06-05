2f74f26a032c4bd8e3a76d97d25cbab575a95a99
mbaddar <mbaddar2@gmail.com>
2023-06-05 19:16:29 +0200

    sandbox example for qa

--- sandbox/qa_sandbox.py
+++ sandbox/qa_sandbox.py
@@ -7,24 +7,17 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 
 if __name__ == '__main__':
-    model_name = "text-curie-001"
-    temperature = 0.7
+    model_name = "text-davinci-003"
+    temperature = 0.2
     llm = OpenAI(model_name=model_name, temperature=temperature)
-    # --
-    # repo_id = "google/flan-t5-xxl"
-    # repo_id = "stabilityai/stablelm-tuned-alpha-3b" # TODO, model got stuck !
-    # repo_id = "databricks/dolly-v2-3b"
-    # llm = HuggingFaceHub(repo_id=repo_id, model_kwargs={"temperature": 0.7, "max_length": 64})
-    #
-    # context_file = open("../sample_data/sample1/faq1.txt", "r")
-    # context_text = context_file.read()
+
     context_text = \
         """
         Q:What is your return and refund policy?
         \n\n
         A : You can return you order with full refund in up to 15 days after the delivery day.
         If you want to return it after 15 days and up to 100 days since delivery you eligible for a partial refund.
-        After that the package is not returnable.
+        After 100 days packages are nor returnable or refundable.
         \n\n
         """
     text_splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap=20)
@@ -36,7 +29,7 @@ if __name__ == '__main__':
     # query = "Do you allow Cashier's Checks or Money Orders?"  # q1 : original question
     # query = "Is it allowed to use Cashier's Checks?" # q1.1 -> success
     query1 = "How can I return my order"  # q1.2 -> failed
-    query2 = "Can I return my order after 100 days?"
+    query2 = "Can I return my order after 105 days?"
     query = query2
     docs = docsearch.get_relevant_documents(query)
     #
@@ -51,4 +44,4 @@ if __name__ == '__main__':
     print(f"query = {query}")
     print(f'Answer without context = \n{res_without_context}')
     print(f'Answer with context without sources = \n{res_with_context_without_sources}')
-    print(f"Answer with context with sources = \n{res_with_context_with_sources['output_text']}")
+    # print(f"Answer with context with sources = \n{res_with_context_with_sources['output_text']}")
