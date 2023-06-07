87c3a2d790914d30cf071b4ba8856b1a8319c023
mbaddar <mbaddar2@gmail.com>
2023-06-07 10:19:42 +0200

    added support to zip file and some cmd coloring

--- sandbox/qa_demo.py
+++ sandbox/qa_demo.py
@@ -5,7 +5,11 @@ https://docs.langchain.com/docs/components/chains/index_related_chains
 import argparse
 import logging
 import os
-
+import zipfile
+from termcolor import colored
+from colorama import init as colorama_init
+from colorama import Fore
+from colorama import Style
 from langchain import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.embeddings import OpenAIEmbeddings
@@ -13,27 +17,30 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
 
 
-def get_context_text(root_dir):
+def get_context_text(zip_file_path):
+    logger = logging.getLogger()
     context_text = ""
-    for root, dirs, files in os.walk(root_dir):
-        for file_name in files:
+    with zipfile.ZipFile(zip_file_path, "r") as zip_file:
+        for file_name in zip_file.namelist():
             if file_name.endswith('.txt'):
-                print(f'Getting file {file_name} in context')
-                file = open(os.path.join(root, file_name))
-                text = file.read()
+                logger.info(f'Appending text from {os.path.join(zip_file_path, file_name)} to context string')
+                text = zip_file.read(file_name).decode()
                 context_text += text
                 context_text += "\n---\n"
+            else:
+                logger.warning(f'file {file_name} is not a text file, and I am not processing it')
     return context_text
 
 
 def get_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--context-root', type=str, required=True, help='Root for context text files')
+    parser.add_argument('--context-zip-path', type=str, required=True, help='Root for context zip file')
     return parser
 
 
-if __name__:
-    logging.basicConfig(level=logging.INFO)
+if __name__ == '__main__':
+    colorama_init()
+    logging.basicConfig(level=logging.CRITICAL)  # hack to disable logging
     logger = logging.getLogger()
     chunk_size = 300
     chunk_overlap = 20
@@ -41,47 +48,34 @@ if __name__:
     parser = get_parser()
     args = parser.parse_args()
     # build chain
-    print("Loading LLM Model")
+    print("Loading LLM Model ###")
     model_name = "text-davinci-003"
     temperature = 0.2
     llm = OpenAI(model_name=model_name, temperature=temperature)
-    context_text = get_context_text(root_dir=args.context_root)
+    context_text = get_context_text(zip_file_path=args.context_zip_path)
     text_splitter = CharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
     texts = text_splitter.split_text(context_text)
     chain = load_qa_chain(llm, chain_type="stuff")
     embeddings = OpenAIEmbeddings()
+    print('Indexing context docs')
     docsearch = Chroma.from_texts(texts, embeddings,
                                   metadatas=[{"source": str(i)} for i in range(len(texts))]).as_retriever()
-    # TODO later
-    # prompt_examples = examples = [
-    #     {
-    #         "question": "How can I convert to e-Sim ?",
-    #         "answer":
-    #             """
-    #             Are follow up questions needed here: Yes.
-    #             Follow up: What is your e-mail?
-    #             Intermediate answer: My e-mail is mbaddar2@gmail.com.
-    #             Follow up: What is your phone Model?
-    #             Intermediate answer: By looking up in the context, my Phone Model is Apple 12.
-    #             Follow up: Is Phone Model Apple 12 compatible with e-Sim?
-    #             Intermediate answer: By looking up in the context, Yes Apple 12 is compatible with e-Sim.
-    #             So the final answer is: As you phone is Compatible with e-Sim, you can convert to e-Sim easily by
-    #             downloading the app.
-    #             """
-    #     }]
     while True:
-        print('input question')
+        # print(colored('Input question', 'red'))
+        print(f"{Fore.GREEN}Input question : {Style.RESET_ALL}!")
         question = input()
         # Generic LLM
         answer1 = llm.predict(question)
-        print(f'Answer with Generic LLM:\n{answer1.strip()}\n===')
+        print(f'{Fore.BLUE}Answer with Generic LLM:\n{answer1.strip()}\n==={Style.RESET_ALL}')
         # Context-Aware LLM-Chain
         docs = docsearch.get_relevant_documents(question)
         answer2 = chain.run(input_documents=docs, question=question)
-        print(f'Answer with Context-Aware LLM:\n{answer2.strip()}')
+        print(f'{Fore.YELLOW}Answer with Context-Aware LLM:\n{answer2.strip()}\n==={Style.RESET_ALL}')
         # Context-Aware Prompt-Engineered LLM
         answer3 = "Under-Construction!!!!"
-        print(f'Answer with Context-Aware Prompt-Engineered LLM:\n{answer3.strip()}\n===')
+        print(
+            f'{Fore.MAGENTA}Answer with Context-Aware Prompt-Engineered LLM:\n{answer3.strip()}\n===\n{Style.RESET_ALL}')
+
         ###
         print('Do you want to ask another question : Y/N ?\n===')
 
